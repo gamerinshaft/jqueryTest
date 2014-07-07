2fa16dcfac50b4c27bc25c412820f02925c7974e
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-07-07 18:10:08 +0900

    スクロールが上か下か判断するコードを書いた

--- first.js
+++ first.js
@@ -1,19 +1,40 @@
 function header(){
-
   obj = $('#target');
-
+  header = $('#header');
+  _window = $(window);
+  nowPosition = _window.scrollTop();
   var target = {
     top: obj.offset().top,
-    left: obj.offset().left
+    left: obj.offset().left,
+    height: obj.height()
+  }
+  var settings = {
+    through: 30 //targetをどれくらいすぎてから出現させるか
   }
 
-  function hikaku(){
-    console.log($(window).scrollTop());
-    console.log(target.top);
-    if($(window).scrollTop() > target.top ){
-      console.log('おおきいよ');
+  function main(){
+    diffPosition = _window.scrollTop();
+    if(diffPosition - nowPosition > 0){
+      console.log('sita');
+      //下にスクロル
+    }else{
+      //上にスクロル
+      console.log('ue');
     }
+    nowPosition = diffPosition;
+
+
+    // if($(window).scrollTop() > target.top + settings.through ){
+    //   header.css({'display':'block', 'position':'fixed'});
+    //   header.animate({
+    //     'top' : '0'
+    //   });
+    //   console.log('おおきい');
+    // }else{
+    //   header.css({'position':'fixed','top' : -(target.height + 1)});
+    //   console.log('ちいさい');
+    // }
   }
-  $(window).on('load resize scroll', hikaku);
+  $(window).on('load resize scroll', main);
 }
 
