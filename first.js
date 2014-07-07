b1ec092bb1290d4a670c027a6de4c96dbb55d2a3
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-07-07 17:10:06 +0900

    定位置を超えたかどうかの判断をする関数を作成した

--- first.js
+++ first.js
@@ -1,5 +1,15 @@
 function target(){
   var obj = $('#target');
+  oya = this;  var oya = this;
   this.top = obj.offset().top;
   this.left = obj.offset().left;
+  function hikaku(){
+    console.log($(window).scrollTop());
+    console.log(oya.top);
+    if($(window).scrollTop() > oya.top ){
+      console.log('おおきいよ');
+    }
+  }
+  $(window).on('load resize scroll', hikaku);
 }
+
