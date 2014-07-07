c4b200a9146e2c9cfba703a8e1b15f7c8fc626f1
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2014-07-08 02:34:35 +0900

    final update

--- simpleScroll.js
+++ simpleScroll.js
@@ -7,6 +7,10 @@ Header = function(elms){
   var lastPosition   = $window.scrollTop();
   var targetThrough  = elms.through;
   var latestPosition = $window.scrollTop();
+
+  $('.ss-header-none').css({'top' : -(header.height() + 1)});
+  $('.ss-header-exist').css({'top' : 0});
+
   var isLowerThanTarget = function(){
     return $(window).scrollTop() > target.offset().top + targetThrough;
   }
@@ -17,26 +21,34 @@ Header = function(elms){
 
   var hideHeader = function(){
     animating    = true;
+    header.addClass('ss-header-animating');
     var params   = {'top' : -(header.height() + 1)};
     var duration = 400;
     var easing   = "swing";
     var complete = function(){
       existing  = false;
       animating = false;
+      header.addClass('ss-header-none')
+      header.removeClass('ss-header-exist');
+      header.removeClass('ss-animating');
     };
-    header.animate(params, duration, easing, complete());
+    $('.ss-header-exist').animate(params, duration, easing, complete);
   }
 
   var showHeader = function(){
     animating    = true;
+    header.addClass('ss-animating');
     var params   = {'top' : '0'};
     var duration = 400;
     var easing   = "swing";
     var complete = function(){
      existing  = true;
      animating = false;
+     header.addClass('ss-header-exist');
+     header.removeClass('ss-header-none')
+     header.removeClass('ss-header-animating');
     };
-    header.animate(params, duration, easing, complete());
+    $('.ss-header-none').animate(params, duration, easing, complete);
   }
 
   this.onScroll = function(){
